2ee79ad597b79e626cbf254e0369736fc66e95fd
bobbyhadz <opmftw@gmail.com>
2024-04-08 14:42:31 +0300

    initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+# Get the length of a 2D Array in Python
+
+my_arr = [[1, 2, 3], [4, 5, 6]]
+
+# ✅ Get the number of rows
+rows = len(my_arr)
+print(rows)  # 👉️ 2
+
+# ✅ Get the number of columns
+cols = len(my_arr[0])
+print(cols)  # 👉️ 3
+
+# ✅ Get the number of total elements
+total_elements = rows * cols
+print(total_elements)  # 👉️ 6
